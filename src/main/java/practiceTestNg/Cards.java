745b161468d0502b42aacb88fe45bf45a01ae1ae
ganipragada <pragadaganesh132@gmail.com>
2025-03-05 13:07:40 +0530

    y commit into develop

--- src/main/java/practiceTestNg/Cards.java
+++ src/main/java/practiceTestNg/Cards.java
@@ -16,6 +16,21 @@ public class Cards {
 	public void goldenCard()
 	{
 		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		System.out.println("goldencard @Test");
+		 
+		
+		
+		
 		
 	}
 	@BeforeTest(groups= {"Regression"})
